1d74d6393649d5967a6cbe6fb55c267db0ac5b47
Amiya Rana <ranaamiya70@gmail.com>
2022-10-08 18:23:52 +0530

    new code

--- terraform_project/launch-template.sh
+++ terraform_project/launch-template.sh
@@ -15,8 +15,8 @@ docker pull 877760304415.dkr.ecr.ap-south-1.amazonaws.com/spring-boot-employee-a
 docker run -p 8080:8080 --name employee-spring-docker-ec2-container 877760304415.dkr.ecr.ap-south-1.amazonaws.com/spring-boot-employee-app:latest
 
 # then go to browser paste public dns like this
-# http://ec2-13-127-148-84.ap-south-1.compute.amazonaws.com:8080/
-# http://ec2-13-127-148-84.ap-south-1.compute.amazonaws.com:8080/employees
+# http://ec2-65-2-187-253.ap-south-1.compute.amazonaws.com:8080/
+# http://ec2-65-2-187-253.ap-south-1.compute.amazonaws.com:8080/employees
 docker stop employee-spring-docker-ec2-container && docker rm --force employee-spring-docker-ec2-container
 
 
